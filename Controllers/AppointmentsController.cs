f65245eb70ae9ce795d484006d690fa6aa3fe0e2
sefamalkoc <132900753+sefamalkoc@users.noreply.github.com>
2025-12-08 19:08:25 +0300

    Controller eklendi

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessCenter.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
